8d927eaddb3f5c5ea00c791f63249206f1819ff0
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-09-01 02:12:17 +0500

    base home done

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { HomeScreen } from "./screens/HomeScreen";
-import { SettingsScreen } from "./screens/SettingsScreen";
+import HomeScreen from "./screens/HomeScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          header: () => null,
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused ? "ios-flask-sharp" : "ios-flask-outline";
+            } else if (route.name === "Settings") {
+              iconName = focused ? "ios-layers" : "ios-layers-outline";
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
       </Tab.Navigator>
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,83 @@
-import * as React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  TouchableOpacity,
+} from "react-native";
+import LiquidProgress from "react-native-liquid-progress";
+import Svg, { Path } from "react-native-svg";
 
-export function HomeScreen() {
+export default function Home() {
+  const [value, setValue] = useState(0);
+  console.log(value);
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home!</Text>
+    <View style={styles.container}>
+      <LiquidProgress
+        backgroundColor={"#ffffff"}
+        frontWaveColor={"#f26541"}
+        backWaveColor={"#edb33c"}
+        fill={value}
+        size={300}
+      >
+        <Animated.View style={styles.row}>
+          <Svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={40}
+            height={40}
+            style={{ marginBottom: 10 }}
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke={(value * 100).toFixed(2) <= 60 ? "#f26541" : "white"}
+            strokeWidth={2}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-droplet"
+          >
+            <Path d="M12 2.69l5.66 5.66a8 8 0 11-11.31 0z" />
+          </Svg>
+          <Text
+            style={{
+              color: (value * 100).toFixed(2) <= 45 ? "#f26541" : "white",
+              fontSize: 43,
+            }}
+          >
+            {(value * 100).toFixed(2)}%
+          </Text>
+        </Animated.View>
+      </LiquidProgress>
+      <View style={styles.buttonLayer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => setValue(Math.random())}
+        >
+          <Text style={styles.buttonText}>Done</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f8f7f1",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  row: { alignItems: "center", marginLeft: 30 },
+  buttonLayer: { flex: 0.5, flexDirection: "row" },
+  button: {
+    width: 140,
+    height: 50,
+    backgroundColor: "#f26541",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+    margin: 30,
+  },
+  buttonText: { fontSize: 15, color: "white" },
+  title: { fontSize: 40, flex: 0.5, color: "gray" },
+});
